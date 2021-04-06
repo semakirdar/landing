d23f71d443fc018a6271b08b1c68985d5f549991
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-06 15:40:43 +0300

    Html,Css,Bootstrap

---
+++ landing3/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="http://localhost:8081/landing3/">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <title>landing3</title>
+</head>
+<body>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="i"><img src="images/vagcomLogo.png"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Services</a>
+                    <a class="nav-link" href="#">Vehicle Models </a>
+                    <a class="nav-link" href="#">Blog</a>
+                    <a class="nav-link" href="#">Locations</a>
+                    <a class="nav-link" href="#">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</div>
+
+
---
+++ landing3/index.php
@@ -0,0 +1,137 @@
+<?php include "header.php"; ?>
+
+    <section>
+        <div class="container">
+            <div class="hero">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="hero-form ">
+                            <div class="form-control">
+                                <select class="form-select mb-4 mt-4" aria-label="Default select example">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                                <div class="mb-4">
+                                    <label for="exampleFormControlInput1" class="form-label">Ad</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                           placeholder="">
+                                </div>
+                                <div class="mb-4">
+                                    <label for="exampleFormControlInput1" class="form-label">Soyad</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                           placeholder="">
+                                </div>
+                                <button type="button" class="btn btn-danger btn-reservation mb-5">CONTINUE CAR
+                                    RESERVATION
+                                </button>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="car-text mb-5">
+                            <h3>LUXURY CAR FROM FROM $28 DAY</h3>
+                            <h5>Treat yourself in USA</h5>
+                        </div>
+                        <div class="car ">
+                            <img class="img-fluid" src="images/car.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service mt-5 mb-5">
+                <h3 class="text-center ">Customer Services</h3>
+                <div class="row mt-5 ">
+                    <div class="col-md-6">
+                        <div class="service-box">
+                            <i class="fas fa-plus-square"></i>
+                            <span>Special rates on car booking</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.A accusantium delectus
+                                enim
+                                exercitationem labore numquam, officiis optio praesentium quia quod repellat
+                                reprehenderit temporibus ullam veniam veritatis?Accusantium dolorem reiciendis
+                                saepe!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="service-box">
+                            <i class="fas fa-plus-square"></i>
+                            <span>Special rates on car booking</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.A accusantium delectus
+                                enim
+                                exercitationem labore numquam, officiis optio praesentium quia quod repellat
+                                reprehenderit temporibus ullam veniam veritatis?Accusantium dolorem reiciendis
+                                saepe!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-6">
+                        <div class="service-box">
+                            <i class="fas fa-plus-square"></i>
+                            <span>Special rates on car booking</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.A accusantium delectus
+                                enim
+                                exercitationem labore numquam, officiis optio praesentium quia quod repellat
+                                reprehenderit temporibus ullam veniam veritatis?Accusantium dolorem reiciendis
+                                saepe!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="service-box">
+                            <i class="fas fa-plus-square"></i>
+                            <span>Special rates on car booking</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.A accusantium delectus
+                                enim
+                                exercitationem labore numquam, officiis optio praesentium quia quod repellat
+                                reprehenderit temporibus ullam veniam veritatis?Accusantium dolorem reiciendis
+                                saepe!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="social">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="social-text ">
+                            <h2>SIGN UP FOR AMAZING OFFERS</h2>
+                            <h5>EXCLUSIVE ACCESS FOR OFFERS AND PROMOTIONS</h5>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="input-group social-input text-center">
+                            <input type="text" class="form-control" placeholder="" aria-label="Recipient's username"
+                                   aria-describedby="button-addon2">
+                            <button class="btn btn-outline-secondary btn-social" type="button" id="button-addon2">
+                                SEND
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="social-icons text-center">
+                            <i class="fab fa-twitter-square twitter"></i>
+                            <i class="fab fa-google-plus-square google"></i>
+                            <i class="fab fa-facebook-square facebook"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="star mt-5 text-center">
+                <i class="fas fa-star small"></i>
+                <i class="fas fa-star small"></i>
+                <i class="fas fa-star big"></i>
+                <i class="fas fa-star small"></i>
+                <i class="fas fa-star small"></i>
+            </div>
+
+
+        </div>
+    </section>
+
+<?php include "footer.php"; ?>
