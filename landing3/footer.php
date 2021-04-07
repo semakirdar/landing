c69b326c2c25433208141a0bb291091a98c2a638
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-07 14:01:07 +0300

    html, css, bootstrap

--- landing3/footer.php
+++ landing3/footer.php
@@ -1,16 +1,15 @@
 <footer>
     <div class="container">
-
-        <div class="footer-navbar text-center mt-5 mb-5">
+        <div class="footer-navbar text-center mt-5 mb-0">
             <a href="#" class="me-3">Home</a>
             <a href="#" class="me-3">Service</a>
             <a href="#" class="me-3">Vehicle Models</a>
             <a href="#" class="me-3">Reviews</a>
             <a href="#" class="me-3">Locations</a>
             <a href="#" class="me-3">Contact</a>
+            <div>2014 Themeinjection</div>
         </div>
-        <div class="footer-black">
-
+        <div class="footer-black mt-0">
         </div>
     </div>
 </footer>
