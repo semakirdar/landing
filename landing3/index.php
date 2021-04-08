a15449ef916b9aab0d1a497237a76681749a4f36
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-08 10:35:28 +0300

    css , html , bootstrap

--- landing3/index.php
+++ landing3/index.php
@@ -32,12 +32,39 @@
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-7">
-                        <div class="car-text mb-5">
-                            <h3>LUXURY CAR FROM FROM $28 DAY</h3>
-                            <h5>Treat yourself in USA</h5>
-                        </div>
                         <div class="car ">
-                            <img class="img-fluid" src="images/car.png">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-indicators">
+                                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+
+                                </div>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <div class="car-text mb-5">
+                                            <h3>LUXURY CAR FROM FROM $28 DAY</h3>
+                                            <h5>Treat yourself in USA</h5>
+                                        </div>
+                                        <img src="images/car.png" class="d-block w-100" alt="...">
+
+                                    </div>
+                                    <div class="carousel-item">
+                                        <div class="car-text mb-5">
+                                            <h3>Get 15% off your rental</h3>
+                                            <h5>Plan your trip now</h5>
+                                        </div>
+                                        <img src="images/slidercar.png" class="d-block w-100" alt="...">
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
