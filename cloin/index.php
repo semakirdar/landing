8673d6dcc36bee56ddcc708a2e14cee50e01c902
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-09 13:56:04 +0300

    html, css, bootstrap

--- cloin/index.php
+++ cloin/index.php
@@ -8,15 +8,29 @@
                         <h3 class="mb-3">We Do Professional Things</br>
                             to Grow Your Business
                         </h3>
-                        <h5>We care to deliver and carft adaptive services and solutions</br>
+                        <h5 class="mb-3">We care to deliver and carft adaptive services and solutions</br>
                             to our customers to achieve and satisfy their needs.We</br>
                             always take attention to every details within work.</h5>
-                        <button type="button" class="btn btn-lg btn-primary btn-start">Let's Start</button>
+                        <button type="button" class="btn btn-lg btn-primary btn-start mt-5">Let's Start</button>
                     </div>
                 </div>
-                <div class="col-sm-12 col-md-12 col-lg-6">
+                <div class="col-sm-12 col-md-12 col-lg-6 ">
                     <div class="hero-form">
-
+                        <div class="form-control">
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="exampleFormControlInput1"
+                                       placeholder="Name">
+                            </div>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="exampleFormControlInput1"
+                                       placeholder="Email">
+                            </div>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="exampleFormControlInput1"
+                                       placeholder="Phone Number">
+                            </div>
+                            <button type="button" class="btn btn-lg btn-primary btn-start mt-5">Submit</button>
+                        </div>
                     </div>
                 </div>
             </div>
