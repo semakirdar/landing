f6c7fce6412b7ae342f390011d67df3a220680a6
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-09 13:01:09 +0300

    html, scss, bootstrap

---
+++ cloin/footer.php
@@ -0,0 +1,10 @@
+<footer>
+
+
+</footer>
+
+
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
---
+++ cloin/index.php
@@ -0,0 +1,25 @@
+<?php include "header.php"; ?>
+<section>
+    <div class="hero">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-6">
+                <div class="hero-image">
+                    <h4>WE CRAFT STUFFS</h4>
+                    <h3>We Do Professional Things</br>
+                        to Grow Your Business
+                    </h3>
+                    <h5>We care to deliver and carft adaptive services and solutions</br>
+                    to our customers to achieve and satisfy their needs.We</br>
+                    always take attention to every details within work.</h5>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6">
+                <div class="hero-form">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include"footer.php"; ?>
