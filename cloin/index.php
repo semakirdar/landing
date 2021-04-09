d5f2e9608c2c2c7808a379c623fe8ad990329926
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-09 15:01:35 +0300

    html,css,bootstrap

--- cloin/index.php
+++ cloin/index.php
@@ -1,39 +1,63 @@
 <?php include "header.php"; ?>
     <section>
         <div class="hero">
-            <div class="row">
-                <div class="col-sm-12 col-md-12 col-lg-6">
-                    <div class="hero-text">
-                        <h5 class="mb-3">WE CRAFT STUFFS</h5>
-                        <h3 class="mb-3">We Do Professional Things</br>
-                            to Grow Your Business
-                        </h3>
-                        <h5 class="mb-3">We care to deliver and carft adaptive services and solutions</br>
-                            to our customers to achieve and satisfy their needs.We</br>
-                            always take attention to every details within work.</h5>
-                        <button type="button" class="btn btn-lg btn-primary btn-start mt-5">Let's Start</button>
-                    </div>
+            <nav class="navbar navbar-light bg-light">
+                <div class="container">
+                    <a class="navbar-brand" href="#">
+                        <img src="images/logo-header.png" alt="">
+                    </a>
                 </div>
-                <div class="col-sm-12 col-md-12 col-lg-6 ">
-                    <div class="hero-form">
-                        <div class="form-control">
-                            <div class="mb-3">
-                                <input type="email" class="form-control" id="exampleFormControlInput1"
-                                       placeholder="Name">
+            </nav>
+            <div class="hero-content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-8">
+                            <div class="hero-text">
+                                <h5 class="mb-3">WE CRAFT STUFFS</h5>
+                                <h3 class="mb-3">We Do Professional Things</br>
+                                    to Grow Your Business
+                                </h3>
+                                <h5 class="mb-3">We care to deliver and carft adaptive services and solutions</br>
+                                    to our customers to achieve and satisfy their needs.We</br>
+                                    always take attention to every details within work.</h5>
+                                <button type="button" class="btn btn-lg btn-primary btn-start mt-3">Let's Start</button>
                             </div>
-                            <div class="mb-3">
-                                <input type="email" class="form-control" id="exampleFormControlInput1"
-                                       placeholder="Email">
+                        </div>
+                        <div class="col-sm-12 col-md-12 col-lg-4 ">
+                            <div class="hero-form">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="form-text text-center mb-5">
+                                            <div class="icon-box">
+                                                <i class="fas fa-bullhorn icon-bullhorn"></i>
+                                            </div>
+                                            <h4 class="mt-4">Get Started Today</h4>
+                                            <p>Everything you need to grow your businness.</br>
+                                                Order our services today!</p>
+                                        </div>
+                                        <div class="mb-3">
+                                            <input type="email" class="form-control" id="exampleFormControlInput1"
+                                                   placeholder="Name">
+                                        </div>
+                                        <div class="mb-3">
+                                            <input type="email" class="form-control" id="exampleFormControlInput1"
+                                                   placeholder="Email">
+                                        </div>
+                                        <div class="mb-3">
+                                            <input type="email" class="form-control" id="exampleFormControlInput1"
+                                                   placeholder="Phone Number">
+                                        </div>
+                                        <div class="d-grid">
+                                            <button type="button" class="btn btn-lg btn-primary btn-submit ">Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="mb-3">
-                                <input type="email" class="form-control" id="exampleFormControlInput1"
-                                       placeholder="Phone Number">
-                            </div>
-                            <button type="button" class="btn btn-lg btn-primary btn-submit ">Submit</button>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     </section>
 
