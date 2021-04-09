fc9d95ea27cbfdb7122c2293f25057fc561ffab0
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-09 13:35:14 +0300

    html, scss, bootstrap

--- cloin/index.php
+++ cloin/index.php
@@ -1,25 +1,26 @@
 <?php include "header.php"; ?>
-<section>
-    <div class="hero">
-        <div class="row">
-            <div class="col-sm-12 col-md-12 col-lg-6">
-                <div class="hero-text">
-                    <h4>WE CRAFT STUFFS</h4>
-                    <h3>We Do Professional Things</br>
-                        to Grow Your Business
-                    </h3>
-                    <h5>We care to deliver and carft adaptive services and solutions</br>
-                    to our customers to achieve and satisfy their needs.We</br>
-                    always take attention to every details within work.</h5>
+    <section>
+        <div class="hero">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-6">
+                    <div class="hero-text">
+                        <h5 class="mb-3">WE CRAFT STUFFS</h5>
+                        <h3 class="mb-3">We Do Professional Things</br>
+                            to Grow Your Business
+                        </h3>
+                        <h5>We care to deliver and carft adaptive services and solutions</br>
+                            to our customers to achieve and satisfy their needs.We</br>
+                            always take attention to every details within work.</h5>
+                        <button type="button" class="btn btn-lg btn-primary btn-start">Let's Start</button>
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-12 col-md-12 col-lg-6">
-                <div class="hero-form">
+                <div class="col-sm-12 col-md-12 col-lg-6">
+                    <div class="hero-form">
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<?php include"footer.php"; ?>
+<?php include "footer.php"; ?>
