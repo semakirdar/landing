553d29dbcdc42f3b6a0c870de882101833315d51
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-13 13:48:10 +0300

    html,scss,bootstrap

--- cloin/index.php
+++ cloin/index.php
@@ -407,5 +407,18 @@
 
             </div>
         </div>
+        <div class="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing </h1>
+                    </div>
+                    <div class="col-md-6">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </section>
 <?php include "footer.php"; ?>
