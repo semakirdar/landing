fde62d7a089ab698e030152397c2503f94d4236f
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-14 14:27:34 +0300

    html,scss, bootstrap

--- a-ha-shop/home.php
+++ a-ha-shop/home.php
@@ -1,55 +1,63 @@
-<?php include"header.php"; ?>
-<section>
-    <div class="container">
-        <div class="slider">
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="images/slider-img1.jpg" class="d-block w-100" alt="...">
+<?php include "header.php"; ?>
+    <section>
+        <div class="container">
+            <div class="slider">
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                                class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                                aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                                aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+                                aria-label="Slide 4"></button>
                     </div>
-                    <div class="carousel-item">
-                        <img src="images/slider-img2.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="images/slider-img1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/slider-img2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/slider-img3.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/slider-img4.jpg" class="d-block w-100" alt="...">
+                        </div>
                     </div>
-                    <div class="carousel-item">
-                        <img src="images/slider-img3.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/slider-img4.jpg" class="d-block w-100" alt="...">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </div>
-        <div class="new-arrivals">
-            <h2>NEW ARRIVALS</h2>
-            <div class="row">
-                <div class="col-md-3">
-                    <img src="#">
-                </div>
-                <div class="col-md-3">
-                    <img src="#">
-                </div>
-                <div class="col-md-3">
-                    <img src="#">
-                </div>
-                <div class="col-md-3">
-                    <img src="#">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
+            <div class="new-arrivals">
+                <h2>NEW ARRIVALS</h2>
+                <div class="shop">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="images/shop_item_1.jpg">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="images/shop_item_2.jpg">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="images/shop_item_3.jpg">
+                        </div>
+                        <div class="col-md-3">
+                            <img src="images/shop_item_4.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 <?php include "footer.php"; ?>
