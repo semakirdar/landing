bc5a4caaa7d56e17771a4e3773b6b7952be2fdaf
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-14 17:01:14 +0300

    html,scss,bootstrap

--- a-ha-shop/footer.php
+++ a-ha-shop/footer.php
@@ -1,7 +1,17 @@
 <footer>
+    <div class="container">
+        <p>Copyright 2021 Cloin.All Rights Reserved</p>
+        <div class="icons">
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-youtube"></i>
+        </div>
+    </div>
 
 </footer>
+
 
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
--- a-ha-shop/header.php
+++ a-ha-shop/header.php
@@ -2,115 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="http://localhost:8081/a-ha-shop/">
+    <link href="http://localhost:8081/cloin/">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
-
-    <title>A-ha Shop</title>
+    <title>Cloin</title>
 
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light nav-top">
-    <div class="container">
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">My Account</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">My Wishlist</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Newsletter</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
 
-        </div>
-    </div>
-</nav>
 
-<div class="container">
-    <div class="nav-middle">
-        <div class="line ">
-        </div>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="nav-search">
-                    <form class="d-flex">
-                        <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="text text-center">
-                    <h1>A-HA SHOP</h1>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="shopping-cart text-end">
-                    <span>CART / </span>
-                    <span> $1299.50</span>
-                    <i class="fas fa-shopping-cart"></i>
-                </div>
-            </div>
-        </div>
-        <div class="line ">
-        </div>
-    </div>
-</div>
 
-<nav class="navbar navbar-expand-lg navbar-light nav-lower ">
-    <div class="container">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
-                aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse " id="navbarScroll">
-            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll mx-auto" style="--bs-scroll-height: 100px;">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="home.php">HOME</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">PAGES</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">CATEGORIES</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">BLOG</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">ELEMENTS</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button"
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        SHOP
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<div class="container">
-<div class="line"></div>
-</div>
+
+
+
 
