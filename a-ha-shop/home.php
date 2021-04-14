0d7d1416acd1b99a925e4825da19d3ba470d3f9c
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-14 15:33:12 +0300

    html,scss,bootstrap

--- a-ha-shop/home.php
+++ a-ha-shop/home.php
@@ -81,7 +81,7 @@
                     </div>
                     <div class="col-md-6">
                         <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                            <input type="text" class="form-control" placeholder="Your Email" aria-label="Recipient's username" aria-describedby="button-addon2">
                             <button class="btn btn-outline-secondary btn-subscribe" type="button" id="button-addon2">Subscribe</button>
                         </div>
                     </div>
