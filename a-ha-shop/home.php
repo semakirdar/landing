33d262659cb11dda07672f1c143f54a0026d6f0a
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-15 15:13:51 +0300

    html,scss

--- a-ha-shop/home.php
+++ a-ha-shop/home.php
@@ -2,54 +2,7 @@
     <section>
         <div class="container">
             <div class="banner">
-                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                                class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                                aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                                aria-label="Slide 3"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
-                                aria-label="Slide 4"></button>
-                    </div>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="images/slider-img1.jpg" class="d-block w-100" alt="...">
-                            <div class="banner-text text-center">
-                                <div class="line mb-4">
-
-                                </div>
-                                <h1 class="mb-2">Collection 2021</h1>
-                                <h5>HOT AND FRESH TRENDS OF THIS YEAR</h5>
-                                <div class="line mt-4">
-
-                                </div>
-                                <button type="button" class="btn btn-light btn-shop mt-5">SHOP NOW</button>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="images/slider-img2.jpg" class="d-block w-100" alt="...">
-
-                        </div>
-                        <div class="carousel-item">
-                            <img src="images/slider-img3.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="images/slider-img4.jpg" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
+               
             </div>
             <div class="new-arrivals">
                 <h2 class="text-center">NEW ARRIVALS</h2>
