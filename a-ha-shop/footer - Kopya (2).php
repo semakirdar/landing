2cc9b3401180e71c3b0c47a0d45120e00844856a
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-16 14:14:13 +0300

    hmtl,scss,bootstrap

---
+++ a-ha-shop/footer - Kopya (2).php
@@ -0,0 +1,73 @@
+<footer>
+    <div class="container">
+        <div class="line"></div>
+        <div class="row">
+            <div class="col-md-2">
+                <h3>INFORMATION</h3>
+                <p>Our stores</p>
+                <p>About us</p>
+                <p>Business with us</p>
+                <p>Delivery information</p>
+                <p>Terms & Condition</p>
+            </div>
+            <div class="col-md-2">
+                <h3>HELP</h3>
+                <p>Contact us</p>
+                <p>track order</p>
+                <p>F.A.Q</p>
+                <p>Privacy policy</p>
+                <p>Returns</p>
+            </div>
+            <div class="col-md-2">
+                <h3>ACCOUNT</h3>
+                <p>My account</p>
+                <p>Wishlist</p>
+                <p>Order history</p>
+                <p>Special gifts</p>
+                <p>Track order</p>
+            </div>
+            <div class="col-md-3">
+                <h3>ABOUT US</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet incidunt neque perferendis placeat
+                    porro reprehenderit rerum, totam. Aspernatur delectus deserunt dolorum enim eveniet facilis labore
+                    laudantium minus sint veniam! Voluptas.</p>
+            </div>
+            <div class="col-md-3">
+                <h3>CONTACT</h3>
+                <p>A-ha Shop is a very slick and clean</p>
+                <p>Phone:+123456789</p>
+                <p>Email:Themesupport@gmail.com</p>
+                <div class="footer-icon mt-4">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"> <i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="#"> <i class="fab fa-linkedin-in"></i></a>
+                    <a href="#"> <i class="fab fa-vuejs"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="line"></div>
+        <div class="row mt-5">
+            <div class="col-md-6">
+                2021 Aha Shop,Made By DeoThemes
+            </div>
+            <div class="col-md-6">
+                <div class="Atm-card-icons text-end">
+                    <i class="fab fa-cc-visa"></i>
+                    <i class="fab fa-cc-jcb"></i>
+                    <i class="fab fa-cc-mastercard"></i>
+                    <i class="fab fa-cc-discover"></i>
+                    <i class="fab fa-cc-amazon-pay"></i>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</footer>
+<script src="plugins/jquery/jquery-3.6.0.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="plugins/OwlCarousel2/dist/owl.carousel.min.js"></script>
+<script src="main.js"></script>
+</body>
+</html>
