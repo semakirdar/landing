e0608d7cdf9bcc794e231d721c19ac3e590ba205
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-16 14:01:31 +0300

    html,scss,bootstrap

---
+++ a-ha-shop/catalog-grid - Kopya.php
@@ -0,0 +1,73 @@
+<?php include "header.php"; ?>
+
+    <section>
+        <div class="container">
+            <div class="filters">
+                <div class="categories mb-5">
+                    <h5>CATEGORIES</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Women</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Men</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Accessories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Bags</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Watches</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Shoes</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="select-size mb-5">
+                    <h5>SELECT SIZE</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Large</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Medium</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Small</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">X-Large</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">X-Small</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="select-color">
+                    <h5>SELECT COLOR</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">White</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Grey</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Black</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pink</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Green</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php include "footer.php"; ?>
---
+++ a-ha-shop/main - Kopya.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.partners-slider').owlCarousel({
+        loop: true,
+        margin: 10,
+        items: 6
+    })
+});
+$(function () {
+    $('.banner-slider').owlCarousel({
+        loop: true,
+        margin: 10,
+        items: 1
+    })
+});
