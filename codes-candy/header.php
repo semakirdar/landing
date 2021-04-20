e1b6cee353ca932343e9e3db895d7c811e2516ad
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-20 12:29:21 +0300

    html,scss,bootstrap

--- codes-candy/header.php
+++ codes-candy/header.php
@@ -6,23 +6,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
-    <title>Document</title>
+    <link href="styles/app.css" rel="stylesheet">
+    <title>CODES CANDY</title>
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false">
                         Browse
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -38,17 +39,20 @@
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false">
                         Landings
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item disabled" href="#" tabindex="-1" aria-disabled="true">LANDINGS</a></li>
+                        <li><a class="dropdown-item disabled" href="#" tabindex="-1" aria-disabled="true">LANDINGS</a>
+                        </li>
                         <li><a class="dropdown-item" href="#">Courses</a></li>
                         <li><a class="dropdown-item" href="#">Lead Course</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false">
                         Pages
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -60,7 +64,9 @@
                         <li><a class="dropdown-item" href="#">Course List/Grid</a></li>
                         <li><a class="dropdown-item" href="#">Add New Course</a></li>
                         <li><a class="dropdown-item" href="#">Paths</a></li>
-                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
                         <li><a class="dropdown-item" href="#">Blog</a></li>
                         <li><a class="dropdown-item" href="#">About</a></li>
                         <li><a class="dropdown-item" href="#">Help Center</a></li>
@@ -68,15 +74,19 @@
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false">
                         Accounts
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item  disabled" href="#" tabindex="-1" aria-disabled="true" >ACCOUNTS</a></li>
+                        <li><a class="dropdown-item  disabled" href="#" tabindex="-1" aria-disabled="true">ACCOUNTS</a>
+                        </li>
                         <li><a class="dropdown-item" href="#">Instructor</a></li>
                         <li><a class="dropdown-item" href="#">Students</a></li>
                         <li><a class="dropdown-item" href="#">Admin</a></li>
-                        <li><hr class="dropdown-divider"></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
                         <li><a class="dropdown-item" href="#">Sign In</a></li>
                         <li><a class="dropdown-item" href="#">Sign Up</a></li>
                         <li><a class="dropdown-item" href="#">Forgot Password</a></li>
@@ -90,12 +100,13 @@
                         <li><a class="dropdown-item" href="#"></a></li>
                     </ul>
                 </li>
-
+                <li class="nav-item ms-4">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                </li>
             </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <ul class="text-end">
+                <
+            </ul>
         </div>
     </div>
 </nav>
--- codes-candy/index.php
+++ codes-candy/index.php
@@ -1,7 +1,26 @@
-<section>
-    <div class="container">
-
-
-
-    </div>
-</section>
+<?php include "header.php" ?>
+    <section>
+        <div class="hero bg-primary">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-6">
+                        <div class="hero-text text-white mt-5 ">
+                            <p>Lorem ipsum dolor sit amet </br> ipsum dolor sit amet aliquam</p>
+                            <span>
+                                Lorem ipsum dolor sit amet, consectetur  adipisicing </br> elit. A aliquam assumenda
+                                consequuntur debitis.consequuntur debitis.
+                            </span>
+                        </div>
+                        <div class="hero-button mt-4 mb-5">
+                            <button type="button" class="btn btn-success btn-lg">Browse Courses</button>
+                            <button type="button" class="btn btn-light btn-lg">Are You Instructor?</button>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-6">
+                        <img src="images/hero-img.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php include "footer.php" ?>
