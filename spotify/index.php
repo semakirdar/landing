557772c5cff029753078e6889a5b6db957c342fa
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-22 11:08:40 +0300

    html

---
+++ spotify/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="http://localhost:8081/spotify/">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="style.scss" rel="stylesheet">
+    <title>Spotify Mobile</title>
+</head>
+<body>
+<div class="container">
+
+
+
+    <section>
+        <div class="options">
+            <div class="btn-angle">
+                <i class="fas fa-angle-down"></i>
+            </div>
+            <div>
+                Liked Songs
+            </div>
+            <div class="btn-ellipsis">
+                <i class="fas fa-ellipsis-h"></i>
+            </div>
+        </div>
+    </section>
+
+
+
+</div>
+</body>
+</html>
