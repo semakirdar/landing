0b20f25f504fff6f2d54515fdee30b95eda978c8
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-22 13:39:09 +0300

    html

--- spotify/index.php
+++ spotify/index.php
@@ -14,14 +14,17 @@
 <div class="container">
 
     <div class="small-view">
-        <section class="options">
+        <section class="song-options">
             <div>
                 <a href="#">
                     <i class="fas fa-angle-down"></i>
                 </a>
             </div>
             <div>
-                Liked Songs
+                <p>
+                    Liked Songs
+                </p>
+
             </div>
             <div>
                 <a href="#">
@@ -29,7 +32,7 @@
                 </a>
             </div>
         </section>
-        <section class="image">
+        <section class="song-image">
             <img src="images/songs-img.jpg">
         </section>
         <section class="song-name">
@@ -37,8 +40,61 @@
                 <h2>Before I Forget</h2>
                 <h4>Slipknot</h4>
             </div>
+            <div class="song-fav">
+                <a>
+                    <i class="far fa-heart"></i>
+                </a>
+            </div>
+        </section>
+        <section class="play-range">
+            <input type="range" min="1" max="200" value="50" class="slider" id="myRange">
+            <div class="times">
+                <div class="current-time">
+                    0:00
+                </div>
+                <div class="full-time">
+                    4:44
+                </div>
+            </div>
+        </section>
+        <section class="player-controls">
+            <div class="btn-small">
+                <a href="#">
+                    <i class="fas fa-random"></i>
+                </a>
+            </div>
             <div>
-                <i class="far fa-heart"></i>
+                <a href="#">
+                    <i class="fas fa-step-backward"></i>
+                </a>
+            </div>
+            <div class="btn-play">
+                <a href="#">
+                    <i class="fas fa-play"></i>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <i class="fas fa-step-forward"></i>
+                </a>
+            </div>
+            <div class="btn-small">
+                <a href="#">
+                    <i class="fas fa-redo"></i>
+                </a>
+            </div>
+        </section>
+        <section class="player-buttons">
+            <div>
+                <a href="#">
+                    <i class="fas fa-desktop"></i>
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <i class="fas fa-sort-amount-up"></i>
+                </a>
+
             </div>
         </section>
     </div>
