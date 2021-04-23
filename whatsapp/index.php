aa3d478f4b65cbc54549634c454fc3b1ec7c6869
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-23 23:37:31 +0300

    html, scss

---
+++ whatsapp/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="html://localhost:8081/whatsapp/">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+
+    <title>Whatsapp</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="small-view">
+
+        <section class="options">
+            <div>
+                Whatsapp
+            </div>
+            <div class="icons">
+                <i class="fas fa-search"></i>
+                
+            </div>
+        </section>
+    </div>
+</div>
+
+
+</body>
+</html>
