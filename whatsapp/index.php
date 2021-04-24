453c0cd5794de10a3a99f032994af938c2b51616
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-24 16:08:40 +0300

    html,scss

--- whatsapp/index.php
+++ whatsapp/index.php
@@ -2,32 +2,46 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="html://localhost:8081/whatsapp/">
+    <link href="http://localhost:8081/whatsapp/">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
-
-    <title>Whatsapp</title>
+    <title>WhatsApp Mobile</title>
 </head>
 <body>
-
 <div class="container">
-    <div class="small-view">
 
+    <div class="small-view">
         <section class="options">
             <div>
-                Whatsapp
+                <p>
+                    WhatsApp
+                </p>
             </div>
             <div class="icons">
-                <i class="fas fa-search"></i>
-                
+                <a class="search-icon">
+                    <i class="fas fa-search"></i>
+                </a>
+                <a class="ellipsis-icon">
+                    <i class="fas fa-ellipsis-h"></i>
+                </a>
+
+            </div>
+        </section>
+        <section class="nav-bar">
+            <div>
+                <i class="fas fa-camera"></i>
+            </div>
+            <div class="nav-items">
+                <a href="#" class="nav-item">SOHBETLER</a>
+                <a href="#" class="nav-item">DURUM</a>
+                <a href="#">ARAMALAR</a>
             </div>
         </section>
     </div>
+
 </div>
-
-
 </body>
 </html>
