8ec7d72b70a29dd64cdcb52e40f2d544293bdb26
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-29 14:27:34 +0300

    html,css

---
+++ instagram/index.php
@@ -0,0 +1,233 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="http://localhost:8081/instagram/">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+
+
+    <title>instagram</title>
+</head>
+<body>
+<div class="container">
+    <div class="small-view">
+
+        <section class="header">
+            <div>
+                instagram
+            </div>
+            <div class="icons">
+                <div class="icon-item">
+                    <i class="far fa-heart"></i>
+                </div>
+                <div class="icon-item">
+                    <div class="messenger-icon">
+                        <i class="fab fa-facebook-messenger"></i>
+                        <div class="number">
+                            <h4>1</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="stories">
+            <div class="story-item">
+                <div class="user-story">
+                    <img src="images/avatar-1.jpg">
+                    <div class="add-icon">
+                        <i class="fas fa-plus"></i>
+                    </div>
+                </div>
+                <p>Hikayen</p>
+            </div>
+            <div class="story-item has-story">
+                <img src="images/avatar-2.jpg">
+                <p>Hikayen</p>
+            </div>
+            <div class="story-item has-story">
+                <img src="images/avatar-3.jpg">
+                <p>Hikayen</p>
+            </div>
+            <div class="story-item has-story">
+                <img src="images/avatar-4.jpg">
+                <p>Hikayen</p>
+            </div>
+        </section>
+        <section class="body">
+            <div class="body-item">
+                <div class="body-options">
+                    <div class="user">
+                        <div>
+                            <img src="images/avatar-4.jpg">
+                        </div>
+                        <div>
+                            saykodelipaylasimlar
+                        </div>
+                    </div>
+                    <div>
+                        <i class="fas fa-ellipsis-v"></i>
+                    </div>
+                </div>
+                <div class="body-shipment">
+                    <img src="images/shipment1.jpg">
+                </div>
+                <div class="body-icons">
+                    <div>
+                        <i class="far fa-heart left"></i>
+                        <i class="far fa-comment left"></i>
+                        <i class="far fa-paper-plane left"></i>
+                    </div>
+                    <div>
+                        <i class="far fa-bookmark"></i>
+                    </div>
+                </div>
+                <div class="body-like">
+                    <div>
+                        <img src="images/avatar-4.jpg">
+                    </div>
+                    <div>
+                        sema kırdar ve 15 kişi bunu beğendi.
+                    </div>
+
+                </div>
+            </div>
+            <div class="body-item">
+                <div class="body-options">
+                    <div class="user">
+                        <div>
+                            <img src="images/avatar-5.jpg">
+                        </div>
+                        <div>
+                            saykodelipaylasimlar
+                        </div>
+                    </div>
+                    <div>
+                        <i class="fas fa-ellipsis-v"></i>
+                    </div>
+                </div>
+                <div class="body-shipment">
+                    <img src="images/shipment2.jpg">
+                </div>
+                <div class="body-icons">
+                    <div>
+                        <i class="far fa-heart left"></i>
+                        <i class="far fa-comment left"></i>
+                        <i class="far fa-paper-plane left"></i>
+                    </div>
+                    <div>
+                        <i class="far fa-bookmark"></i>
+                    </div>
+                </div>
+                <div class="body-like">
+                    <div>
+                        <img src="images/avatar-4.jpg">
+                    </div>
+                    <div>
+                        sema kırdar ve 15 kişi bunu beğendi.
+                    </div>
+
+                </div>
+            </div>
+            <div class="body-item">
+                <div class="body-options">
+                    <div class="user">
+                        <div>
+                            <img src="images/avatar-3.jpg">
+                        </div>
+                        <div>
+                            saykodelipaylasimlar
+                        </div>
+                    </div>
+                    <div>
+                        <i class="fas fa-ellipsis-v"></i>
+                    </div>
+                </div>
+                <div class="body-shipment">
+                    <img src="images/shipment3.jpg">
+                </div>
+                <div class="body-icons">
+                    <div>
+                        <i class="far fa-heart left"></i>
+                        <i class="far fa-comment left"></i>
+                        <i class="far fa-paper-plane left"></i>
+                    </div>
+                    <div>
+                        <i class="far fa-bookmark"></i>
+                    </div>
+                </div>
+                <div class="body-like">
+                    <div>
+                        <img src="images/avatar-4.jpg">
+                    </div>
+                    <div>
+                        sema kırdar ve 15 kişi bunu beğendi.
+                    </div>
+
+                </div>
+            </div>
+            <div class="body-item">
+                <div class="body-options">
+                    <div class="user">
+                        <div>
+                            <img src="images/avatar-2.jpg">
+                        </div>
+                        <div>
+                            saykodelipaylasimlar
+                        </div>
+                    </div>
+                    <div>
+                        <i class="fas fa-ellipsis-v"></i>
+                    </div>
+                </div>
+                <div class="body-shipment">
+                    <img src="images/shipment4.jpg">
+                </div>
+                <div class="body-icons">
+                    <div>
+                        <i class="far fa-heart left"></i>
+                        <i class="far fa-comment left"></i>
+                        <i class="far fa-paper-plane left"></i>
+                    </div>
+                    <div>
+                        <i class="far fa-bookmark"></i>
+                    </div>
+                </div>
+                <div class="body-like">
+                    <div>
+                        <img src="images/avatar-4.jpg">
+                    </div>
+                    <div>
+                        sema kırdar ve 15 kişi bunu beğendi.
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+        <section class="footer">
+            <div>
+                <i class="fas fa-home"></i>
+            </div>
+            <div>
+                <i class="fas fa-search"></i>
+            </div>
+            <div>
+                <i class="far fa-plus-square"></i>
+            </div>
+            <div>
+                <i class="fas fa-shopping-basket"></i>
+            </div>
+            <div>
+                <img src="images/avatar-1.jpg">
+            </div>
+        </section>
+    </div>
+
+
+</body>
+</html>
---
+++ upcoming-events/index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="http://localhost:8081/upcoming-events/">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,300;0,400;0,700;1,500&display=swap"
+          rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <title>UPCOMING EVENTS</title>
+</head>
+<body>
+
+<div class="container">
+    <section>
+        <h1>UPCOMING EVENTS</h1>
+        <div class="events">
+            <div class="event-item">
+                <div class="image">
+                    <img src=https://images.unsplash.com/photo-1607332742884-3cea04bf9a6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5NDI4NjEyfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60">
+                </div>
+                <div class="text">
+                    <div class="time">
+                        <div class="day">23</div>
+                        <div class="month-year">
+                            <div>MARCH</div>
+                            <div>2022</div>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <div>SOUND SPIRIT</div>
+                    </div>
+                    <div class="place">
+                        PLACE 128 - LONDON, UK
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="event-item">
+                <div class="image">
+                    <img src="https://images.unsplash.com/photo-1565377004729-d6b83ebf8d6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60">
+                </div>
+                <div class="text">
+                    <div class="time">
+                        <div class="day">11</div>
+                        <div class="month-year">
+                            <div>MAY</div>
+                            <div>2022</div>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <div>TECHNO FEST</div>
+
+                    </div>
+                    <div class="place">
+                        PLACE 128 - LONDON, UK
+                    </div>
+                </div>
+
+            </div>
+            <div class="event-item">
+                <div class="image">
+                    <img src="https://images.unsplash.com/photo-1558961166-9c584702dcb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80">
+                </div>
+                <div class="text">
+                    <div class="time">
+                        <div class="day">18</div>
+                        <div class="month-year">
+                            <div>APRIL</div>
+                            <div>2022</div>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <div>CITY AT NIGHT</div>
+
+                    </div>
+                    <div class="place">
+                        PLACE 128 - LONDON, UK
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+</body>
+</html>
