78ee6d5ec9b23ffe981fb34d91bd6d32db49d692
sema <72384075+semakirdar@users.noreply.github.com>
2021-04-30 17:31:15 +0300

    html,scss,bootstrap

--- franshals-museum/index.php
+++ franshals-museum/index.php
@@ -2,12 +2,232 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="http://localhost:8081/franshals-museum/">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="plugins/OwlCarousel2/dist/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="plugins/OwlCarousel2/dist/assets/owl.theme.default.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <title>franshals-museum</title>
 </head>
 <body>
+<section class="hero">
+    <div class="hero-title">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-7 text-end">
+                <div class="hero-logo">
+                    <h4>FRANS HALS</h4>
+                    <h4 class="rotate"> MUSEUM</h4>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-5 text-end">
+                <div class="play-btn">
+                    <span class="down">P</span>
+                    <span>L</span>
+                    <span class="down">A</span>
+                    <span>Y</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h1 class="text fw-bold">WELCOME</h1>
+    <div class="hero-slider">
+
+        <div id="carouselExampleIndicators" class="carousel-slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="slide-text text-center">
+                        <h3>deneme</h3>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="slide-text text-center">
+                        <h3>Lorem ipsum dolor sit amet</h3>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="slide-text text-center">
+                        <h3>Lorem ipsum dolor sit amet, consectetur</h3>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="slide-text text-center">
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="hero-btn">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <button type="button" class="btn btn-dark btn-lg btn-buy">BUY TICKETS</button>
+            </div>
+            <div class="col-md-12 text-center">
+                <button type="button" class="btn btn-lg btn-all">ALL EXHIBITIONS</button>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="exhibitions">
+    <div class="exhibitions-slider owl-carousel owl-theme">
+        <div class="exhibition-item">
+            <div class="card">
+                <img src="images/exhibition.jpg">
+                <div class="card-body">
+                    <div class="body-text">
+                        <h5>Maatregelen en andere veelgestelde vragen</h5>
+                    </div>
+                    <div class="body-ticket-icon">
+                        <i class="far fa-calendar-check"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="exhibition-item">
+            <div class="card">
+                <img src="images/exhibition1.jpg">
+                <div class="card-body text-center">
+                    25 jun 2021 - 02 jan 2022
+                    <div class="body-ticket-icon">
+                        <i class="far fa-calendar-check"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="exhibition-item">
+            <div class="card">
+                <img src="images/exhibition2.jpg">
+                <div class="card-body">
+                    <div class="body-ticket-icon">
+                        <i class="far fa-calendar-check"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="exhibition-item">
+            <div class="card">
+                <img src="images/exhibition3.jpg">
+                <div class="card-body text-center">
+                    19 Sep - 2020 01 Jul 2021
+                    <div class="body-ticket-icon">
+                        <i class="far fa-calendar-check"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="exhibition-item">
+            <div class="card">
+                <img src="images/exhibition4.png">
+                <div class="card-body">
+                    <div class="body-ticket-icon">
+                        <i class="far fa-calendar-check"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<section class="footer">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <div class="bulletin text-center">
+                <span>
+                    NIEUWSBRIEF
+                </span>
+                <h3 class="text-center my-4">Frans stuurt je graag e-mails</h3>
+                <div class="form">
+                    <div class="row">
+                        <div class="col-md-6 text-end">
+                            <input type="password" placeholder="Naam">
+                        </div>
+                        <div class="col-md-6">
+                            <input type="email" placeholder="E-mail">
+                        </div>
+                    </div>
+                    <div class=" form-btn text-center my-4">
+                        <button class="btn btn-dark btn-sign-up text-center" type="submit">aanmelden</button>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <div class="location text-center">
+                <span>
+                    ÉÉN MUSEUM OP TWEE LOCATIES
+                </span>
+                <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-6 ">
+                        <i class="fas fa-warehouse my-4"></i>
+                        <h4>HOF</h4>
+                        <h5>Groot Heiligland
+                            62 2011 ES Haarlem
+                            <h5 class="underline">
+                                Openingstijden
+                            </h5>
+                        </h5>
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-6">
+                        <i class="fab fa-fort-awesome my-4"></i>
+                        <h4>HAL</h4>
+                        <h5>Groot Heiligland
+                            62 2011 ES Haarlem
+                            <h5 class="underline">
+                                Openingstijden
+                            </h5>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="nav">
+        <div class="social-icons">
+            <i class="fab fa-facebook-f"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-youtube"></i>
+            <i class="fab fa-twitter"></i>
+        </div>
+        <div class="nav-bar">
+            <a href="#">
+                Over ons
+            </a>
+            <a href="#">
+                About as
+            </a>
+            <a href="#">
+                Veelgestelde vragen <br/>
+                Curatoren
+            </a>
+            <a href="#">
+                Contact
+            </a>
+            <a href="#">
+                Colofon
+            </a>
+        </div>
+        <div class="footer-logo">
+
+        </div>
+    </div>
+</section>
+
+
+<script src="plugins/jquery/jquery-3.6.0.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="plugins/OwlCarousel2/dist/owl.carousel.min.js"></script>
+<script src="main.js"></script>
 
 </body>
 </html>
---
+++ franshals-museum/main.js
@@ -0,0 +1,22 @@
+
+/*$(function () {
+    $('.carousel-slide').owlCarousel({
+        loop: true,
+        margin: 10,
+        items: 4,
+        dots:true
+
+
+    })
+});*/
+
+
+$(function () {
+    $('.exhibitions-slider').owlCarousel({
+        loop: true,
+        margin: 50,
+        items: 3,
+        dots:false
+
+    })
+});
