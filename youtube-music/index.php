86a48edab03ed26ded8cb6b55c68961d5ac79eae
sema <sema.kirdar@ogr.dpu.edu.tr>
2021-05-04 16:59:15 +0300

    html,scss,bootstrap

---
+++ youtube-music/index.php
@@ -0,0 +1,388 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="http://localhost/youtube-music/">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="plugins/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <title>Youtube</title>
+</head>
+<body>
+<div class="view">
+    <div class="container">
+        <div class="navbar pt-3">
+            <div>
+                <a href="#">Music</a>
+            </div>
+
+            <div class="nav-mid">
+                <div class="mid-text">
+                    <a class="ms-3" href="#">Anasayfa</a>
+                    <a class="ms-3" href="#">Keşfet</a>
+                    <a class="ms-3" href="#">Kitaplık</a>
+                </div>
+                 <div class="search">
+                     <i class="fas fa-search"></i>
+                     <span>Arayın</span>
+                 </div>
+            </div>
+
+            <div class="navbar-image">
+                <img src="images/nav-avatar.jpg">
+            </div>
+
+
+        </div>
+        <div class="content mt-5">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-7">
+                    <div class="song-image text-center">
+                        <img src="images/content-img.jpeg" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-5">
+                    <div class="song-list">
+                        <nav>
+                            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home"
+                                        aria-selected="true">SIRADAKİ
+                                </button>
+                                <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-profile" type="button" role="tab"
+                                        aria-controls="nav-profile" aria-selected="false">ŞARKI SÖZLERİ
+                                </button>
+
+                            </div>
+                        </nav>
+                        <div class="tab-content pt-2" id="nav-tabContent">
+                            <div class="tab-pane fade show active" id="nav-home" role="tabpanel"
+                                 aria-labelledby="nav-home-tab">
+                                <div class="tab-item">
+
+                                    <div class="item ">
+                                        <div class="tab-img ">
+                                            <img src="images/song-image.jpeg" class="img-fluid ">
+
+                                            <div class="playing-layer">
+                                                <i class="fas fa-volume-up"></i>
+                                            </div>
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">Alive</h5>
+                                            <h5>Pearl Jam</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image1.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">Down</h5>
+                                            <h5>stone temple pilots</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image2.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">tumble in the rough</h5>
+                                            <h5>stone temple pilots </h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image3.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">Facelift</h5>
+                                            <h5>Alice in chains</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image4.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">Because of the times</h5>
+                                            <h5>kings of leon</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image5.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">jeremy</h5>
+                                            <h5>Kings of leon</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image6.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white"> californication</h5>
+                                            <h5>red hot chili peppers</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image7.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">Jeremy</h5>
+                                            <h5>Pearl Jam</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">Alive</h5>
+                                            <h5>Pearl Jam
+                                            </h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image1.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">Down</h5>
+                                            <h5>stone temple pilots</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image2.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">tumble in the rough</h5>
+                                            <h5>stone temple pilots </h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image3.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">Facelift</h5>
+                                            <h5>Alice in chains</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image4.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">Because of the times</h5>
+                                            <h5>kings of leon</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image5.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">jeremy</h5>
+                                            <h5>Kings of leon</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                                <div class="tab-item">
+
+                                    <div class="item">
+                                        <div class="tab-img img-fluid">
+                                            <img src="images/song-image7.jpeg">
+                                        </div>
+                                        <div class="tab-text">
+                                            <h5 class="text-white">jeremy</h5>
+                                            <h5>Kings of leon</h5>
+                                        </div>
+                                    </div>
+
+                                    <div class="time">
+                                        <h5>5:41</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="nav-profile" role="tabpanel"
+                                 aria-labelledby="nav-profile-tab">
+
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto autem
+                                doloremque eius esse, excepturi magni nostrum numquam officiis omnis sint voluptates?
+                                Est facere ipsum molestias nobis non! Dicta, quas!
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores cum cumque
+                                debitis, dolor doloribus ea id incidunt laborum odit officiis perferendis perspiciatis
+                                quas quasi sapiente sed sunt veritatis voluptas.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam assumenda, dicta
+                                dolor enim iusto laudantium maxime non obcaecati quis suscipit totam. Culpa harum illo
+                                molestias pariatur, recusandae sed? Blanditiis.
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="fixed">
+            <div class="song-footer">
+                <div class="song-play">
+                    <div>
+                        <i class="fas fa-step-backward me-4"></i>
+                    </div>
+                    <div class="pause">
+                        <i class="fas fa-pause me-4"></i>
+                    </div>
+                    <div>
+                        <i class="fas fa-step-forward me-4"></i>
+                    </div>
+                    <div class="text">
+                        <h5>0:39 /</h5>
+                    </div>
+                    <div class="text">
+                        <h5>5:43</h5>
+                    </div>
+                </div>
+                <div class="song">
+                    <div class="img-fluid">
+                        <img src="images/content-img.jpeg" class="me-4">
+                    </div>
+                    <div class="text me-4">
+                        <h5 class="text-white">Alive</h5>
+                        <h5>Pearl Jam</h5>
+                    </div>
+                    <div>
+                        <i class="far fa-thumbs-down me-4"></i>
+                    </div>
+                    <div class="up">
+                        <i class="fas fa-thumbs-up me-4"></i>
+                    </div>
+                    <div>
+                        <i class="fas fa-ellipsis-v"></i>
+                    </div>
+
+                </div>
+                <div class="song-options">
+                    <div>
+                        <i class="fas fa-volume-up me-4"></i>
+                    </div>
+                    <div>
+                        <i class="fas fa-random me-4"></i>
+                    </div>
+                    <div>
+                        <i class="fas fa-random me-4"></i>
+                    </div>
+                    <div class="down">
+                        <i class="fas fa-caret-down"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
